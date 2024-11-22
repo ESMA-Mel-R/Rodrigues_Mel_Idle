e39644df6a800b48d248a3ca3e23b98cc22daf10
ESMA-MelR <m.rodrigues@rns.ecolescreatives.com>
2024-11-22 16:14:16 +0100

    protoclicker

--- Assets/scripts/Get points.cs
+++ Assets/scripts/Get points.cs
@@ -7,5 +7,27 @@ using TMPro;
 public class Getpoints : MonoBehaviour
 {
     [SerializeField] private TMP_Text score;
+    private float currentScore;
+    private float addScore;
+
+    private void Start()
+    {
+        currentScore = 0;
+        addScore = 1;
+    }
+
+    private void Update()
+    {
+        score.text = currentScore.ToString();
+    }
+
+    public void IncreaseScore()
+    {
+        currentScore = currentScore + addScore;
+    }
     
+    public void MultiplierScore()
+    {
+        addScore += 1;
+    }
 }
